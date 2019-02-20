c785945d4143bd8b6b629aefc916313970f1ec95
nenadV91 <nenadwebdevelop@gmail.com>
2019-02-20 15:10:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Navbar from 'components/navbar';
+import Home from 'pages/home';
+import Item from 'pages/item';
+
+const NotFound = () => <div>Page not found</div>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+        
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/item" component={Item} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
     );
   }
