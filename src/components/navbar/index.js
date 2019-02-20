d3509e33165052f20b3fb473b26fb9f462bdeb22
nenadV91 <nenadwebdevelop@gmail.com>
2019-02-20 17:22:08 +0100

    Home page - basics completed.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from 'assets/imgs/logo.gif';
 
 class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+      <nav className="navbar">
+        <Link className="logo" to="/">
+          <img className="logo-img" src={logo} alt=""/>
+          <strong>Hacker news</strong>
+        </Link>
+      </nav>
     );
   }
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Layout from 'components/layout'
+import Story from 'components/story';
 
 class Home extends Component {
+  state = {
+    news: null,
+    size: 30,
+    page: 0
+  }
+
+  fetchStories = async ids => {
+    let { page, size } = this.state;
+    const start = page * size;
+    const base = 'https://hacker-news.firebaseio.com/v0';
+    const input = ids.slice(start, start + size).map(id => `${base}/item/${id}.json`);
+
+    try {
+      const data = await Promise.all(input.map(link => axios.get(link)))
+      return data.map(({ data }) => data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  componentWillMount = async () => {
+    const base = 'https://hacker-news.firebaseio.com/v0';
+    const path = '/topstories.json'
+
+    try {
+      const ids = await axios.get(`${base}/${path}`);
+      const news = await this.fetchStories(ids.data)
+      this.setState({ news })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  renderNews = () => {
+    const { news } = this.state;
+
+    if(news === null) {
+      return 'Loading news...'
+    }
+
+    if(!news.length) {
+      return 'Nothing found.'
+    }
+
+    return news.map((item, index) =>
+      <Story index={index + 1} key={item.id} data={item} />
+    )
+  }
+
+
   render() {
     return (
-      <div>Home page</div>
+      <Layout>
+        {this.renderNews()}
+      </Layout>
     );
   }
 }
